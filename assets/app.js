49b55a49ce70374bc44ecb5400f59d41f1f221be
procoderalamin <prodeveloperalamin@gmail.com>
2022-01-14 21:44:50 +0600

    first commit

---
+++ assets/app.js
@@ -0,0 +1,4 @@
+const name = document.querySelector("#name");
+const year = document.querySelector("#year");
+const result = document.querySelector("#result");
+const send = document.querySelector("#send");
